c9e7e477ffe9b16581c405f9bb7b9912a61cce40
nikita <vodenikovni@mosreg.ru>
2025-10-12 11:37:00 +0300

    feat: tg api + google sheets

--- src/entities/enroll/model/index.ts
+++ src/entities/enroll/model/index.ts
@@ -8,27 +8,11 @@ import {
 import { useState } from "react";
 import { format } from "date-fns";
 import { SLIDES_DATA } from "./consts";
+import { BRANCHES_DETAIL_DATA } from "@/shared/consts/branches-detail-data";
 
 interface UseEnrollModelProps {
   categoryKey?: string;
 }
-
-// const url =
-//   "https://script.google.com/macros/s/AKfycbxcR9OZUfEM969wzLW6mKYfMnHedgmxNE30xIitgLf1K4Qq8ayfs0Ot5NMBlR7G4sgh/exec";
-
-// const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-//   e.preventDefault();
-//   fetch(url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-//     },
-//     body: `FullName=${encodeURIComponent(e.target.fullName.value)}&Email=${encodeURIComponent(e.target.email.value)}&Phone=${encodeURIComponent(e.target.phone.value)}&Service=${encodeURIComponent(e.target.service.value)}&Comment=${encodeURIComponent(e.target.comment.value)}`,
-//   })
-//     .then((res) => res.text())
-//     .then((data) => alert(data))
-//     .catch((error) => console.log(error));
-// };
 
 export const useEnrollModel = ({ categoryKey }: UseEnrollModelProps) => {
   const [fetching, setFetching] = useState(false);
@@ -60,20 +44,46 @@ export const useEnrollModel = ({ categoryKey }: UseEnrollModelProps) => {
     },
   });
 
-  const onSubmit = form.handleSubmit((values) => {
+  const BOT_TOKEN = "7844701764:AAHm-GruIJ6mJSxwIEVFeWORfpx9D4hu4Po";
+  const CHAT_ID = "424493509";
+
+  const onSubmit = form.handleSubmit(async (values) => {
     setFetching(true);
 
     const variables = {
       ...values,
+      categoryLabel:
+        values.chooseCategoryForm.categoryKey === "any"
+          ? "Другое"
+          : BRANCHES_DETAIL_DATA[values.chooseCategoryForm.categoryKey].name,
       date: format(new Date(), "dd.MM.yyyy HH:mm"),
     };
 
-    console.log(variables);
+    const url =
+      "https://script.google.com/macros/s/AKfycbxcR9OZUfEM969wzLW6mKYfMnHedgmxNE30xIitgLf1K4Qq8ayfs0Ot5NMBlR7G4sgh/exec";
 
-    setTimeout(() => {
-      setFetching(false);
-      setStatus("success");
-    }, 1000);
+    const message = encodeURIComponent(`Новая заявка!`);
+    const tgURL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`;
+
+    try {
+      const response = await fetch(tgURL);
+      if (response.ok) {
+        console.log("Успешно!");
+      }
+    } catch (e) {
+      console.error("Ошибка:", e);
+    }
+
+    // fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    //   },
+    //   body: `lastName=${encodeURIComponent(variables.personalDataForm.lastName)}&firstName=${encodeURIComponent(variables.personalDataForm.firstName)}&middleName=${encodeURIComponent(variables.personalDataForm.middleName || "")}&email=${encodeURIComponent(variables.personalDataForm.email)}&phone=${encodeURIComponent(variables.personalDataForm.phone)}&categoryLabel=${encodeURIComponent(variables.categoryLabel)}&comment=${encodeURIComponent(variables.personalDataForm.comment || "")}&categoryDetail=${encodeURIComponent(variables.personalDataForm.categoryDetail || "")}`,
+    // })
+    //   .then((res) => res.text())
+    //   .then((data) => alert(data))
+    //   .catch((error) => console.log(error));
   });
 
   const onOpenChange = (value: boolean) => {
